7e57431f3f559c238c55c7b9d771d8a98dd7cbb7
Andres Rios <rianby64@gmail.com>
2015-12-20 02:51:08 +0300

    added a simple feature for testing and a set of tests for that simple feature

---
+++ spec/apolog.spec.js
@@ -0,0 +1,77 @@
+'use strict';
+// the next test are being performed by using the features from Apolog
+
+var apolog = require('../index.js'),
+    fs = require('fs'),
+    example = {
+      file: {
+        path: 'simpleFeature.feature'
+      },
+      content: fs.readFileSync('./spec/simpleFeature.feature', 'utf8')
+    };
+
+apolog.loadFeature(example.content, example.file);
+describe("it parsers a simple feature in a natural way", function() {
+  var feature = 0,
+      background = 0,
+      background_given = 0,
+      scenario = 0,
+      given = 0,
+      when = 0,
+      then = 0,
+      functions = {
+        feature: function() {
+          feature++;
+          apolog.background('A background', functions.background);
+          apolog.scenario('Simple scenario', functions.scenario);
+        },
+        background: function() {
+          background++;
+          apolog.given('a given background', functions.background_given);
+        },
+        background_given: function() {
+          background_given++;
+        },
+        scenario: function() {
+          scenario++;
+          apolog.given('A given', functions.given);
+          apolog.when('A when', functions.when);
+          apolog.then('A step', functions.then);
+        },
+        given: function() {
+          given++;
+        },
+        when: function() {
+          when++;
+        },
+        then: function() {
+          then++;
+        }
+      };
+  apolog.feature('Simple Feature', functions.feature);
+  apolog.run();
+  it('the feature was called', function() {
+    expect(feature).toBe(1);
+  });
+  it('the background was called', function() {
+    expect(background).toBe(1);
+  });
+  it('the background given step was called', function() {
+    expect(background_given).toBe(1);
+  });
+  it('the scenario was called', function() {
+    expect(scenario).toBe(1);
+  });
+  it('the given was called', function() {
+    expect(given).toBe(1);
+  });
+  it('the when was called', function() {
+    expect(when).toBe(1);
+  });
+  it('the then was called', function() {
+    expect(then).toBe(1);
+  });
+});
+
+
+
---
+++ spec/simpleFeature.feature
@@ -0,0 +1,7 @@
+Feature: Simple Feature
+  Background: A background
+    Given a given background
+  Scenario: Simple scenario
+   Given A given
+   When A when
+   Then A step
