4a96b55724ebe45c824ba214feeab06725ab5804
Andres Rios <rianby64@gmail.com>
2015-12-31 13:59:13 +0300

    simple testing for background

--- spec/features/simpleFeature.feature
+++ spec/features/simpleFeature.feature
@@ -2,6 +2,10 @@ Feature: Simple Feature
   Background: A background
     Given a given background
   Scenario: Simple scenario
-   Given A given
-   When A when
-   Then A step
+    Given A given
+    When A when
+    Then A step
+  Scenario: Another scenario
+    Given another given
+    When another when
+    Then another step
--- spec/simpleFeature.spec.js
+++ spec/simpleFeature.spec.js
@@ -3,7 +3,8 @@
 
 loadFeature('./spec/features/simpleFeature.feature');
 describe("it parsers a simple feature", function() {
-  var execution_map = {};
+  var execution_map = {},
+      errors;
 
   feature('Simple Feature', function feature() {
     execution_map.feature = execution_map.feature || { count: 0 };
@@ -43,18 +44,29 @@ describe("it parsers a simple feature", function() {
         yield {};
       });
     });
+    scenario('Another scenario', function() {
+      given('another given', function() {
+
+      });
+      when('another when', function() {
+
+      });
+      then('another step', function() {
+
+      });
+    });
   });
 
-  run();
+  errors = run();
 
   it('so "feature" was called', function() {
     expect(execution_map.feature.count).toBe(1);
   });
   it('so "background" was called', function() {
-    expect(execution_map.background.count).toBe(1);
+    expect(execution_map.background.count).toBe(2);
   });
   it('so "background given" step was called as normal function', function() {
-    expect(execution_map.background_given.count).toBe(1);
+    expect(execution_map.background_given.count).toBe(2);
   });
   it('so "scenario" was called', function() {
     expect(execution_map.scenario.count).toBe(1);
@@ -67,6 +79,9 @@ describe("it parsers a simple feature", function() {
   });
   it('the then was called as async function* with done() === undefined parameter', function() {
     expect(execution_map.then.count).toBe(1);
+  });
+  it('running without errors', function() {
+    expect(errors.length).toBe(0);
   });
 });
 
