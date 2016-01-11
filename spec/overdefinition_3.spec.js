ba0ad474686243ac598eae4bc213b6b9ee29d392
Andres Rios <rianby64@gmail.com>
2016-01-11 07:36:57 +0300

    last step for testing overdefinitions. Can't finish it

---
+++ spec/overdefinition_3.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+// the next test are being performed by using the features from Apolog
+describe("Feature defined", function() {
+  var errors;
+  loadFeature('./spec/features/overdefined.feature');
+
+  feature(/Simple Feature/, function() {
+    background(/A background/, function() {
+      given(/a given background/, function() {
+        given("no matter what", function() { }); // has no sense
+      });
+    });
+    scenario(/Simple scenario/, function() {
+      given(/A given/, function() {
+        given("no matter what", function() { }); // has no sense
+      });
+    });
+  });
+  errors = run();
+
+  it('runs without errors', function() {
+    expect(errors.length).toBe(0);
+  });
+});
+
